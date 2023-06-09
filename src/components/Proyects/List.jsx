f50179f6892fdcd52efe2fe09f3333bf4fe95226
Samuel <samuelfs96@gmail.com>
2023-06-09 07:42:43 -0400

    add new proyect and update card design

--- src/components/Proyects/List.jsx
+++ src/components/Proyects/List.jsx
@@ -1,54 +1,69 @@
-import { Badge, Card } from 'flowbite-react'
-import React from 'react'
-import proyects from './proyects-data'
+import { Badge, Card } from "flowbite-react";
+import React from "react";
+import proyects from "./proyects-data";
+import github_logo from "../../img/github-mark.png";
 
 const badgeColors = [
-    "info",
-    "gray",
-    "failure",
-    "success",
-    "warning",
-    "indigo",
-    "purple",
-    "pink"
-]
+  "info",
+  "gray",
+  "failure",
+  "success",
+  "warning",
+  "indigo",
+  "purple",
+  "pink",
+];
 
 const list = () => {
   return (
     <div className="container text-sgl-gray mt-24 mb-24">
-        <div className='grid grid-cols-3 max-lg:grid-cols-1 gap-4 container'>
-            {
-                proyects.map(({
-                    title,
-                    description,
-                    skills,
-                    image,
-                    link
-                }, key)=> (
-                    <div className="max-w-sm" key={key}>
-                        <Card imgSrc={image} href={link} target="_blank">
-                            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                {title}
-                            </h5>
-                            <div className="flex flex-wrap gap-2">
-                            {
-                                skills.map((skill, key) => (
-                                    <Badge color={badgeColors[key]} key={key}>
-                                        {skill}
-                                    </Badge>
-                                ))
-                            }
-                            </div>
-                            <p className="font-normal text-gray-700 dark:text-gray-400">
-                                {description}
-                            </p>
-                        </Card>
-                    </div>
-                ))
-            }
-        </div>
+      <div className="grid grid-cols-3 max-lg:grid-cols-1 gap-4 container">
+        {proyects.map(({ title, description, skills, image, link }, key) => (
+          <div className="max-w-sm" key={key}>
+            <Card
+              className="[&>img]:h-[200px] [&>img]:object-cover"
+              imgSrc={image}
+            >
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {title}
+              </h5>
+              <div className="flex flex-wrap gap-2">
+                {skills.map((skill, key) => (
+                  <Badge color={badgeColors[key]} key={key}>
+                    {skill}
+                  </Badge>
+                ))}
+              </div>
+              <p className="font-normal text-gray-700 dark:text-gray-400 break-words">
+                {description}
+              </p>
+              <div className="flex gap-2 items-center justify-end">
+                <a title="Ver demo" className="hover:opacity-70" href={link} target="_blank" rel="noreferrer">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                    />
+                  </svg>
+                </a>
+                <a title="Ver código" href={link} target="_blank" rel="noreferrer" className="w-[27px] hover:opacity-70">
+                  <img src={github_logo} alt="github logo" />
+                </a>
+              </div>
+            </Card>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default list
+export default list;
--- src/components/Proyects/proyects-data.jsx
+++ src/components/Proyects/proyects-data.jsx
@@ -1,80 +1,75 @@
-import pro1 from '../../img/proyects/pro1.png'
-import pro2 from '../../img/proyects/pro2.png'
-import pro3 from '../../img/proyects/pro3.png'
-import pro4 from '../../img/proyects/pro4.png'
-import pro5 from '../../img/proyects/pro5.png'
+import pro1 from "../../img/proyects/pro1.png";
+import pro2 from "../../img/proyects/pro2.png";
+import pro3 from "../../img/proyects/pro3.png";
+import pro4 from "../../img/proyects/pro4.png";
+import pro5 from "../../img/proyects/pro5.png";
+import pro6 from "../../img/proyects/pro6.png";
 
 const proyects = [
-    {
-        "title": "Simulador de Pokédex",
-        "description": "Simulador de Pokédex completa con datos provenientes de la pokeapi: https://pokeapi.co/, cuenta con listado, controles y banda sonora.",
-        "skills": [
-            "html",
-            "css",
-            "javascript",
-            "react",
-            "tailwind"
-        ],
-        "image": `${pro1}`,
-        "link": "https://magnificent-liger-2d9dfc.netlify.app/"
-    },
-    {
-        "title": "Web para Sandstone Global",
-        "description": "Web informativa para la empresa SGL MX. Cuenta un diseño adaptado a todos los dispositivos, cambio de idioma, y conexión al blog.",
-        "skills": [
-            "html",
-            "css",
-            "javascript",
-            "react",
-            "tailwind",
-            "node"
-        ],
-        "image": `${pro2}`,
-        "link": "https://sgl.com.mx/"
-    },
-    {
-        "title": "Web para CJP Telecom",
-        "description": "Web informativa para la empresa CJP Telecom MX, diseño adaptable.",
-        "skills": [
-            "html",
-            "css",
-            "javascript",
-            "react",
-            "tailwind",
-        ],
-        "image": `${pro3}`,
-        "link": "https://cjp.mx/"
-    },
-    {
-        "title": "Prueba Técnica",
-        "description": "Prueba técnica realizada para validar conocimientos, cuenta con listado de productos, usuarios, paginación y un workflow creado con la libreria react flow.",
-        "skills": [
-            "html",
-            "css",
-            "javascript",
-            "react",
-            "tailwind",
-            "react-query",
-            "react-flow"
-        ],
-        "image": `${pro4}`,
-        "link": "https://snazzy-kleicha-e00173.netlify.app/"
-    },
-    {
-        "title": "Lol Champion Stats",
-        "description": "Aplicación para ver datos actualizados del videojuego League of Legends. Consulta la api pública https://ddragon.leagueoflegends.com/, cuenta con filtros, estadísticas y aspectos.",
-        "skills": [
-            "html",
-            "css",
-            "javascript",
-            "react",
-            "nextjs",
-            "chartjs",
-            "material-ui"
-        ],
-        "image": `${pro5}`,
-        "link": "https://lolchampstats-app.vercel.app/"
-    }
-]
+  {
+    title: "Simulador de Pokédex",
+    description:
+      "Simulador de Pokédex completa con datos provenientes de la pokeapi: https://pokeapi.co/, cuenta con listado, controles y banda sonora.",
+    skills: ["html", "css", "javascript", "react", "tailwind"],
+    image: `${pro1}`,
+    link: "https://magnificent-liger-2d9dfc.netlify.app/",
+  },
+  {
+    title: "Web para Sandstone Global",
+    description:
+      "Web informativa para la empresa SGL MX. Cuenta un diseño adaptado a todos los dispositivos, cambio de idioma, y conexión al blog.",
+    skills: ["html", "css", "javascript", "react", "tailwind", "node"],
+    image: `${pro2}`,
+    link: "https://sgl.com.mx/",
+  },
+  {
+    title: "Web para CJP Telecom",
+    description:
+      "Web informativa para la empresa CJP Telecom MX, diseño adaptable.",
+    skills: ["html", "css", "javascript", "react", "tailwind"],
+    image: `${pro3}`,
+    link: "https://cjp.mx/",
+  },
+  {
+    title: "Prueba Técnica",
+    description:
+      "Prueba técnica realizada para validar conocimientos, cuenta con listado de productos, usuarios, paginación y un workflow creado con la libreria react flow.",
+    skills: [
+      "html",
+      "css",
+      "javascript",
+      "react",
+      "tailwind",
+      "react-query",
+      "react-flow",
+    ],
+    image: `${pro4}`,
+    link: "https://snazzy-kleicha-e00173.netlify.app/",
+  },
+  {
+    title: "Lol Champion Stats",
+    description:
+      "Aplicación para ver datos actualizados del videojuego League of Legends. Consulta la api pública https://ddragon.leagueoflegends.com/, cuenta con filtros, estadísticas y aspectos.",
+    skills: [
+      "html",
+      "css",
+      "javascript",
+      "react",
+      "nextjs",
+      "chartjs",
+      "material-ui",
+    ],
+    image: `${pro5}`,
+    link: "https://lolchampstats-app.vercel.app/",
+  },
+  {
+    title: "Frontend Mentor - E-commerce product page challenge",
+    description:
+      "Pagina de producto, realizada para resolver el desafio de frontendmentor https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6 cuenta con diseño adaptable y carrito de compras",
+    skills: ["html", "css", "javascript", "react", "tailwind"],
+    image: `${pro6}`,
+    link: "https://polite-crepe-fdbcd1.netlify.app/",
+  },
+];
 
 export default proyects;
