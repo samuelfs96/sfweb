9e5d6f66e9fe9da4a74a60a61d9ce2be596464cd
Samuel <samuelfs96@gmail.com>
2024-02-15 23:07:51 -0400

    feat: re-design contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,41 @@
 import phone from '../img/telefono.png'
 import email from '../img/email.png'
+import pdf from '../img/pdf.png'
+import pdf_file from '../resources/CV_Samuel_Figueroa_frontendDev.pdf'
 
 const Contact = () => {
   return (
-    <section id="contacto">
-      <h1 className=' p-8 text-3xl text-center font-bold uppercase'>Datos de Contacto</h1>
-      <div className="flex justify-center items-start max-sm:flex-col max-sm:items-center mt-12 mb-12 gap-12">
+    <section id="contacto" className='py-16 bg-gradient-to-tl from-black to-[#0f172a]'>
+      <div className="flex items-center flex-col my-16">
+      <div>
+        <h1
+          className=" text-white text-4xl font-bold m-2 uppercase"
+        >
+          Datos de Contacto
+        </h1>
+        <hr className="border-4 border-sf-blue w-[60%] -skew-x-12" />
+      </div>
+    </div>
+      <div className="flex justify-center items-center">
+        <div>
         <div className="flex items-center gap-4 pb-4">
-          <img src={phone} alt="phone" className='w-20' />
+          <img src={phone} alt="phone" className='w-20 invert' />
           <div>
-            <h2 className="text-2xl text-sgl-black font-bold">Teléfono</h2>
-            <p className="text-sgl-gray font-lato">+584120962677</p>
+            <h2 className="text-2xl font-bold text-white">Teléfono</h2>
+            <p className="font-light text-white">+584120962677</p>
           </div>
         </div>
         <div className="flex items-center gap-4 pb-4">
-          <img src={email} alt="email" className='w-20' />
+          <img src={email} alt="email" className='w-20 invert' />
           <div>
-            <h2 className="text-2xl text-sgl-black font-bold">Email</h2>
-            <p className="text-sgl-gray font-lato">samuelfs96@gmail.com</p>
+            <h2 className="text-2xl font-bold text-white">Email</h2>
+            <p className="font-light text-white">samuelfs96@gmail.com</p>
           </div>
+        </div>
+        <div className="flex items-center gap-4 pb-4 cursor-pointer" onClick={() => window.open(pdf_file)}>
+          <img src={pdf} alt="pdf" className='w-20 invert' />
+            <h2 className="text-2xl font-bold text-white">Ver CV</h2>
+        </div>
         </div>
       </div>
     </section>
