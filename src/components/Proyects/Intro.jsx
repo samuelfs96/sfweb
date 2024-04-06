a48070a2fee17169cc3185c465c3469ebea2119f
Samuel Figueroa <samuelfs96@gmail.com>
2024-04-06 16:04:54 -0400

    feat: :zap: format file

--- src/components/Proyects/Intro.jsx
+++ src/components/Proyects/Intro.jsx
@@ -1,16 +1,12 @@
 const Intro = () => {
-  return (
-    <div className="flex items-center flex-col mt-20">
-      <div>
-        <h1
-          className=" text-white text-4xl font-bold m-2 uppercase"
-        >
-          Mis Proyectos
-        </h1>
-        <hr className="border-4 border-sf-blue w-[60%] -skew-x-12" />
-      </div>
+  <div className="flex items-center flex-col mt-20">
+    <div>
+      <h1 className=" text-white text-4xl font-bold m-2 uppercase">
+        Mis Proyectos
+      </h1>
+      <hr className="border-4 border-sf-blue w-[60%] -skew-x-12" />
     </div>
-  );
+  </div>;
 };
 
 export default Intro;
